1e68ea86ba2f955d92b3217cd3335babf2d33391
Sylvain Roy <sylvain.roy@m4x.org>
2024-05-05 17:43:41 +0200

    Day02

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -4,6 +4,8 @@ use std::path::Path;
 
 fn main() -> io::Result<()> {
     let path = Path::new("./data/input.txt");
+
+    // Part 1
     let file = File::open(&path)?;
     let reader = io::BufReader::new(file);
 
@@ -38,6 +40,41 @@ fn main() -> io::Result<()> {
             sum += game_id;
         }
     }
-    println!("{}", sum);
+    println!("Part 1: {}", sum);
+
+    // Part 2
+    let file = File::open(&path)?;
+    let reader = io::BufReader::new(file);
+
+    let mut total_power = 0;
+    for line in reader.lines() {
+        let line = line?;
+        let parts: Vec<&str> = line.split(": ").collect();
+        let sets: Vec<&str> = parts[1].split("; ").collect();
+        let mut min_red = i32::MIN;
+        let mut min_green = i32::MIN;
+        let mut min_blue = i32::MIN;
+        for set in sets {
+            let cubes: Vec<&str> = set.split(", ").collect();
+            for cube in cubes {
+                let cube_parts: Vec<&str> = cube.split(" ").collect();
+                let count: i32 = cube_parts[0].parse().unwrap();
+                let color = cube_parts[1];
+                if color == "red" {
+                    min_red = min_red.max(count);
+                } else if color == "green" {
+                    min_green = min_green.max(count);
+                } else if color == "blue" {
+                    min_blue = min_blue.max(count);
+                }
+            }
+        }
+        let power = min_red * min_green * min_blue;
+        total_power += power;
+        println!("line: {:?}", line);
+        println!("mins: {:?}, {:?}, {:?}", min_red, min_green, min_blue);
+        println!("power: {:?}", power);
+    }
+    println!("Part 2: {}", total_power);
     Ok(())
 }
